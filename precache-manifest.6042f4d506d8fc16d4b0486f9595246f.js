085a591cefb0d6facbbfc4c6849a2d2aef9f0b73
iamtheasad <iamtheasad@gmail.com>
2019-03-06 08:03:22 +0600

    Updates

---
+++ precache-manifest.6042f4d506d8fc16d4b0486f9595246f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "c466d87f29acc36e316d",
+    "url": "/robot/static/js/runtime~main.c466d87f.js"
+  },
+  {
+    "revision": "a8d07f8c2a0642444219",
+    "url": "/robot/static/js/main.a8d07f8c.chunk.js"
+  },
+  {
+    "revision": "911b7b3acdd24d48c967",
+    "url": "/robot/static/js/1.911b7b3a.chunk.js"
+  },
+  {
+    "revision": "a8d07f8c2a0642444219",
+    "url": "/robot/static/css/main.27882129.chunk.css"
+  },
+  {
+    "revision": "911b7b3acdd24d48c967",
+    "url": "/robot/static/css/1.b3c30155.chunk.css"
+  },
+  {
+    "revision": "7b4053eb24bea5c218d4b1e0e1f2d974",
+    "url": "/robot/index.html"
+  }
+];
